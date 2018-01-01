752f272e99105174b855d32e20d6f0523f0af170
sutton97 <15565166@students.lincoln.ac.uk>
2018-01-01 12:27:03 +0000

    1

--- Js/JavaScript.js
+++ Js/JavaScript.js
@@ -1,45 +1,15 @@
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
 
-//window load
+
+//WINDOW LOAD
 window.onload=function(){
+function getSelectValue()
+{
+	var selectedValue = document.getElementById("drop").value;
+	console.log(selectedValue);
+	}	
+	getSelectValue();	
 	
-	
-/* function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
 }
- */
-// Close the dropdown menu if the user clicks outside of it
-/* window.onclick = function(event) {
-	console.log("test");
-  if (!event.target.matches('.dropbtn')) {
-
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-} */
-$('.dropdown').click(function(){
-	$('#myDropdown').toggle();
-	
-	
-});
-$('#myDropdown a').click(function(){
-	console.log('change class');
-	$('#myDropdown a').removeClass('selected');
-	$(this).addClass('selected');
-	var size = $(this).data('size');
-	console.log(size);
-	
-});
-
-}//leave
-
 
 //CUSTOMISER
 window.onload = init;
