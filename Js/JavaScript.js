866f80eb677e99d93994ee72b3456a1442b92b4b
sutton97 <15565166@students.lincoln.ac.uk>
2018-01-10 13:38:01 +0000

    1

--- Js/JavaScript.js
+++ Js/JavaScript.js
@@ -130,48 +130,16 @@ function check_info(){
 			return true;
 		}
 	}
-//LOCAL STORAGE
-var x = document.getElementById("demo");
-function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition, showError);
-    } else { 
-        x.innerHTML = "Geolocation is not supported by this browser.";
-    }
-}
-
-function showPosition(position) {
-    var lat = position.coords.latitude;
-    var lon = position.coords.longitude;
-    var latlon = new google.maps.LatLng(lat, lon)
-    var mapholder = document.getElementById('mapholder')
-    mapholder.style.height = '250px';
-    mapholder.style.width = '500px';
-
-    var myOptions = {
-    center:latlon,zoom:14,
-    mapTypeId:google.maps.MapTypeId.ROADMAP,
-    mapTypeControl:false,
-    navigationControlOptions:{style:google.maps.NavigationControlStyle.SMALL}
-    }
-    
-    var map = new google.maps.Map(document.getElementById("mapholder"), myOptions);
-    var marker = new google.maps.Marker({position:latlon,map:map,title:"You are here!"});
-}
-
-function showError(error) {
-    switch(error.code) {
-        case error.PERMISSION_DENIED:
-            x.innerHTML = "User denied the request for Geolocation."
-            break;
-        case error.POSITION_UNAVAILABLE:
-            x.innerHTML = "Location information is unavailable."
-            break;
-        case error.TIMEOUT:
-            x.innerHTML = "The request to get user location timed out."
-            break;
-        case error.UNKNOWN_ERROR:
-            x.innerHTML = "An unknown error occurred."
-            break;
-    }
-}
+//GEO-LOCATION
+function initMap(){
+			var location = {lat: -25.363, lng: 131.044};
+			var map = new google.maps.Map(document.getElementById("map"),
+			{zoom: 4, center: location});
+			var marker = new google.maps.Marker({
+				position: location,
+				map: map
+			});
+		}
+	async defer src =
+	"https://maps.googleapis.com/maps/api/js?key=AIzaSyAxxaEsjgpC9bt7Db8DQHa9PHus8WhWU8Y&callback=initMap";
+	
