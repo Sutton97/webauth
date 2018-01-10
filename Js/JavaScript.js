5ada22a4bae558f9eb581c429846f2ae2a8d7ca0
sutton97 <15565166@students.lincoln.ac.uk>
2018-01-10 12:41:17 +0000

    1

--- Js/JavaScript.js
+++ Js/JavaScript.js
@@ -134,12 +134,51 @@ function check_info(){
 function local(){
 	
        if(localStorage.getItem('scentColor')!=null)
-		   img.src =
-	   localStorage.getItem('scentColor');
+		   img.src = localStorage.getItem('scentColor');
 	 if(localStorage.getItem('candleColor')!=null)
-		   img2.src =
-	   localStorage.getItem('candleColor');
+		   img2.src = localStorage.getItem('candleColor');
 }
 
+//GEO LOCATION
+var x = document.getElementById("demo");
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition, showError);
+    } else { 
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+    x.innerHTML = "Latitude: " + position.coords.latitude + 
+    "<br>Longitude: " + position.coords.longitude;
+}
+
+function showError(error) {
+    switch(error.code) {
+        case error.PERMISSION_DENIED:
+            x.innerHTML = "User denied the request for Geolocation."
+            break;
+        case error.POSITION_UNAVAILABLE:
+            x.innerHTML = "Location information is unavailable."
+            break;
+        case error.TIMEOUT:
+            x.innerHTML = "The request to get user location timed out."
+            break;
+        case error.UNKNOWN_ERROR:
+            x.innerHTML = "An unknown error occurred."
+            break;
+    }
+}
+
+function showPosition(position) {
+    var latlon = position.coords.latitude + "," + position.coords.longitude;
+
+    var img_url = "https://maps.googleapis.com/maps/api/staticmap?center=
+    "+latlon+"&zoom=14&size=400x300&sensor=false&key=YOUR_:KEY";
+
+    document.getElementById("mapholder").innerHTML = "<img src='"+img_url+"'>";
+}
 
 
